185771bec8afcbed2daa479751c70937c5b1504a
CamilleINGOUF <ingouf.camille@gmail.com>
2017-12-06 14:47:16 +0100

    commit

--- optimisation_numerique/tp2.R
+++ optimisation_numerique/tp2.R
@@ -6,6 +6,33 @@ derf <- function(x) {
   return(-3*x^2+2*x-2)
 }
 
+f2 <- function(x) {
+  return(10*x^4 - 20*x^3 - 90*x^2 + 20*x + 80)
+}
+
+derf2 <- function(x) {
+  return(40*x^3 - 60*x^2 - 180*x + 20)
+}
+
+der2f2 <- function(x) {
+  return(120*x^2 - 120*x - 180)
+}
+
+derivef <- function(f,h) {
+  df <- function(x) {
+    return((f(x+h) - f(x)) /h)
+  }
+  return (df)
+}
+
+g1 <- function(x) {
+  return(cos(((x-0.5)^2 * (x + 1) + 1)/(x + 1)))
+}
+
+g2 <- function(x) {
+  return (sqrt(exp((x - 0.25)^2)+1))
+}
+
 newton <- function(f, derf, x, precision) {
   success <- FALSE
   dyn <- c()
@@ -18,7 +45,18 @@ newton <- function(f, derf, x, precision) {
 }
 
 main <- function() {
-  newt <- newton(f, derf, 10, 10^-2)
-  cat(newt$x,"\n",f(newt$x))
+  newt <- newton(derf2, der2f2, 4, 10^-8)
   plot(newt$dyn)
+  plot(seq(-100,100,by=0.5),f2(seq(-100,100,by=0.5)))
+  
+  dg1 <- derivef(g1,0.0001)
+  ddg1 <- derivef(dg1,0.0001)
+  x1 <- newton(dg1,ddg1,0,10^-6)
+  
+  dg2 <- derivef(g2,0.0001)
+  ddg2 <- derivef(dg2,0.0001)
+  x2 <- newton(dg2,ddg2,0,10^-6)
+  
+  cat(x1$x," ",x2$x," ",g1(x1$x)+g2(x2$x))
+  plot(mesx, g1(mesx)+g2(mesx))
 }
