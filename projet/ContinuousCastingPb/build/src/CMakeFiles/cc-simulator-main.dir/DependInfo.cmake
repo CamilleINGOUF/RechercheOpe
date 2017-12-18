b7f1bdc1df06fce80e50c4f1c93a5f4885224aec
CamilleINGOUF <ingouf.camille@gmail.com>
2017-12-18 16:36:39 +0100

    projet

---
+++ projet/ContinuousCastingPb/build/src/CMakeFiles/cc-simulator-main.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/src/cluster.cpp" "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/build/src/CMakeFiles/cc-simulator-main.dir/cluster.cpp.o"
+  "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/src/crane.cpp" "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/build/src/CMakeFiles/cc-simulator-main.dir/crane.cpp.o"
+  "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/src/evalCC.cpp" "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/build/src/CMakeFiles/cc-simulator-main.dir/evalCC.cpp.o"
+  "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/src/gantry_crane.cpp" "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/build/src/CMakeFiles/cc-simulator-main.dir/gantry_crane.cpp.o"
+  "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/src/generator.cpp" "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/build/src/CMakeFiles/cc-simulator-main.dir/generator.cpp.o"
+  "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/src/main.cpp" "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/build/src/CMakeFiles/cc-simulator-main.dir/main.cpp.o"
+  "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/src/models.cpp" "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/build/src/CMakeFiles/cc-simulator-main.dir/models.cpp.o"
+  "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/src/run_out_table.cpp" "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/build/src/CMakeFiles/cc-simulator-main.dir/run_out_table.cpp.o"
+  "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/src/stack.cpp" "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/build/src/CMakeFiles/cc-simulator-main.dir/stack.cpp.o"
+  "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/src/stock.cpp" "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/build/src/CMakeFiles/cc-simulator-main.dir/stock.cpp.o"
+  "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/src/utils/rand.cpp" "/etudiants/cingouf/Documents/RO/projet/ContinuousCastingPb/build/src/CMakeFiles/cc-simulator-main.dir/utils/rand.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "../usr/local/include/artis-star-1.0"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ projet/ContinuousCastingPb/src/main.cpp
@@ -0,0 +1,156 @@
+/**
+ * @file main.cpp
+ * See the AUTHORS or Authors.txt file
+ */
+
+/*
+ * Copyright (C) 2017-2018 ULCO http://www.univ-litoral.fr
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <graph_manager.hpp>
+#include <models.hpp>
+
+#include <artis-star/common/RootCoordinator.hpp>
+
+#include <solution.hpp>
+#include <evalCC.hpp>
+
+#include <chrono>
+#include <fstream>
+#include <iostream>
+
+#include <time.h> 
+
+using namespace cc;
+using namespace artis::common;
+
+/*
+class View : public cc::View
+{
+public:
+    View()
+    {
+        selector("Stack:height", { RootGraphManager::CC,
+                    SubGraphManager::STACK, cc::View::ALL, Stack::HEIGHT });
+        selector("Crane:moveNumber", { RootGraphManager::CC,
+                    SubGraphManager::CRANE, Crane::MOVE_NUMBER });
+        selector("Crane:slabNumber", { RootGraphManager::CC,
+                    SubGraphManager::CRANE, Crane::SLAB_NUMBER });
+    }
+};
+
+void stat(const artis::common::RootCoordinator < DoubleTime,
+          artis::pdevs::Coordinator < DoubleTime, RootGraphManager,
+          GlobalParameters > >& rc)
+{
+    unsigned int sum = 0;
+    unsigned int sum_N = 0;
+
+    for (unsigned int i = 1; i <= 5; ++i) {
+        const ::View::Values& values = rc.observer().view("CC").get(
+            "Stack:height", (boost::format(":root:CC:stack_%1%:height") %
+                             i).str());
+
+        for (::View::Values::const_iterator it = values.begin();
+             it != values.end(); ++it) {
+            int height;
+
+            it->second(height);
+            sum += height;
+        }
+        sum_N += values.size();
+    }
+    std::cout << "Average of stack height = " << ((double)sum / sum_N)
+              << std::endl;
+
+    {
+        const ::View::Values& values = rc.observer().view("CC").get(
+            "Crane:moveNumber");
+        int move_number;
+
+        values.back().second(move_number);
+        std::cout << "Crane move number: " << move_number << std::endl;
+    }
+    {
+        const ::View::Values& values = rc.observer().view("CC").get(
+            "Crane:slabNumber");
+        int slab_number;
+
+        values.back().second(slab_number);
+        std::cout << "Total slab number: " << slab_number << std::endl;
+    }
+}
+*/
+void random_solution(Solution & solution) {
+  srand(time(NULL));
+  for(unsigned i = 0; i < solution.size(); i++) {
+    int r = rand() % 101;
+    solution[i] = r;
+  }
+}
+
+int random_search(Solution & solution, int nb_eval_max) {
+  EvalCC eval;
+  
+  Solution buffer = solution;
+  int best_fitness = solution.fitness();
+  for(signed i = 0; i < nb_eval_max; i++) {
+    
+    random_solution(buffer);
+    eval(buffer);
+    int fitness = buffer.fitness();
+    
+    if(fitness < best_fitness) {
+      solution = buffer;
+      best_fitness = fitness;
+    }
+  }
+
+  eval(solution);
+  return best_fitness;
+}
+
+int main()
+{
+  // constants related to the dimension of the optimization problem 
+    const unsigned int n_stack = 5;
+    const unsigned int n_destination = 8;
+    const unsigned int solution_size = n_destination + n_stack * n_destination * (n_destination - 1);
+
+    // evaluation function from the Continuous Casting simulator
+    EvalCC eval;
+
+    // Declaration of one solution of the optimization problem
+    Solution s ;
+
+    // solution for random selection: all stacks have the same preference 1
+    s.resize(solution_size, 1);
+
+    // init the first solution
+    random_solution(s);
+
+    std::ofstream outputfile;
+    outputfile.open("rs.csv");
+    outputfile << "fitness nbeval\n";
+
+    for(unsigned i = 0; i < 20; i++) {
+      random_search(s, 10);
+      outputfile << s.fitness() << " 10\n";
+      std::cout << i << std::endl;
+    }
+
+    return 0;
+}
